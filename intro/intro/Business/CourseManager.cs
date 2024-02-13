c78afaa7c4e32882af5c96ad8c2686de456b86c2
Yusuf Kızgın <117774196+yusufkizgin@users.noreply.github.com>
2024-02-13 08:54:01 +0300

    Add files via upload

---
+++ intro/intro/Business/CourseManager.cs
@@ -0,0 +1,40 @@
+﻿using intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro.Business
+{
+    public class CourseManager
+    {
+        Course[] courses = new Course[2];
+        public CourseManager()
+        {
+            Course course1 = new();
+            course1.Name = "C#";
+            course1.Description = "Bu bir c# kursu";
+            course1.Id = 1;
+            course1.Price = 50;
+
+            Course course2 = new();
+            course2.Name = "JAVA";
+            course2.Description = "Bu bir java kursu";
+            course2.Id = 2;
+            course2.Price = 30;
+
+            courses[0] = course1;
+            courses[1] = course2;
+
+        }
+        public Course[] GetAll()
+        {
+            return courses;
+        }
+    }
+}
+    
+
+   
+
---
+++ intro/intro/Program.cs
@@ -0,0 +1,35 @@
+﻿using intro.Business;
+using intro.Entities;
+
+Console.WriteLine("Hello, World!");
+string mssg1 = "Krediler Buraya";
+int term = 12;
+double ammount = 10000;
+bool isAuthenticated = false;
+Console.WriteLine(mssg1);
+
+/*
+if (isAuthenticated)
+{   
+    Console.WriteLine("Hoşgeldin");
+}
+else
+{
+    Console.WriteLine("Giriş Yap!");
+}
+
+string[] loans = { "Kredi1", "Kredi2", "Kredi3" };
+
+for(int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}*/
+
+CourseManager courseManager1 = new();
+Course[] kurslar = courseManager1.GetAll();
+
+for (int i = 0; i < kurslar.Length; i++)
+{
+    Console.WriteLine(kurslar[i].Name + " / " + kurslar[i].Price);
+}
+
